e2530be9da2b49995f42858808bbab9f3e8b4c0b
oliver <oliver.pecek@gmail.com>
2022-11-28 09:00:43 +0100

    adding new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule
