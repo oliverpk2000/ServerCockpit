0329246ec83f62d5e8730c578478b3bd3e89da44
oliver <oliver.pecek@gmail.com>
2022-11-29 16:20:34 +0100

    refactoring bc homework had a part 2, part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ export class AppComponent {
   serverArray: ServerObject[] = [];
 
 
+  appendServerObject(so: ServerObject) {
+    this.serverArray.push(so);
+  }
 }
 
 
---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,29 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  title = 'ServerCockpit';
+  serverName: String = "";
+  serverType: String = "";
+
+  @Output() objectCreator: EventEmitter<ServerObject> = new EventEmitter<ServerObject>()
+
+  createServer() {
+    let so = new ServerObject()
+    so.name = this.serverName;
+    so.type = this.serverType;
+    this.serverName = "";
+    this.serverType = "";
+    this.objectCreator.emit(so);
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
